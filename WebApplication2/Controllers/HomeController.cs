5de2b3d9f3d0654b5c1279d529661bbcd17214a7
Raphael HaMa <baja.bongo1983@gmail.com>
2018-08-04 21:44:59 +0200

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET /Home/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET /Home/Info
+        public IActionResult Info()
+        {
+            return View();
+        }
+    }
+}
